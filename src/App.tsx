12b944e716c5ac511ec880a54568bcd42a8be97e
CodeSandbox <support@codesandbox.io>
2024-02-04 14:20:44 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+const phrases = [
+  "No",
+  "Are you sure?",
+  "Reallyyy sure?",
+  "Plz dont break my kiddo heart",
+  "Scara kitty :(",
+];
 function App() {
-  const [count, setCount] = useState(0);
+  const [noCount, setCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
 
+  function handleNoClick() {
+    setCount(noCount + 1);
+  }
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="valentine-container">
+      {yesPressed ? (
+        <>
+          <img alt="cat with hearts" src="https://tenor.com/bySXn.gif" />
+          <div className="text">Yayyyyy!!!!!</div>
+        </>
+      ) : (
+        <>
+          <img alt="cat crying" src="https://tenor.com/bhFXP.gif" />
+          <div>Will you be my Sun to my Moon?</div>
+          <div>
+            <button
+              className="yesButton"
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button onClick={handleNoClick} className="noButton">
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
-
 export default App;
